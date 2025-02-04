661da5269b98e14ffdb2d3412c1ff0e32b0c7d0b
Dan Souza <darleyble3@gmail.com>
2025-02-03 22:32:43 -0300

    knowledges finished

---
+++ app/components/pages/home/know-techs/technologies.tsx
@@ -0,0 +1,79 @@
+import { TbBrandNextjs } from "react-icons/tb";
+
+export const technologies = [
+  {
+    icon: <TbBrandNextjs />,
+    name: "HTML5",
+    startDate: "2020-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "CSS3",
+    startDate: "2020-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Javascript",
+    startDate: "2020-09-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Typescript",
+    startDate: "2023-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Bootstrap",
+    startDate: "2020-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Tailwind",
+    startDate: "2023-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Angular",
+    startDate: "2024-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "React",
+    startDate: "2024-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Nextjs",
+    startDate: "2024-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "NodeJs",
+    startDate: "2024-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Express",
+    startDate: "2024-02-03",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Java",
+    startDate: "2023-10-08",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "Spring Boot",
+    startDate: "2023-10-08",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "MySQL",
+    startDate: "2023-10-08",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    name: "PostgreSQL",
+    startDate: "2023-02-03",
+  },
+];
