b0656781fc8b3c7ce7cf0dad231387c93c1cf9f4
Dan Souza <darleyble3@gmail.com>
2025-02-03 22:32:56 -0300

    add knowledges to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-'use client';
+"use client";
 
 import { HeroSection } from "./components/pages/home/hero-section";
+import { KnowTechs } from "./components/pages/home/know-techs";
 
 export default function Home() {
   return (
     <>
+    <div className="bg-hero-image bg-fixed">
       <HeroSection />
+      <KnowTechs />
+    </div>
     </>
   );
 }
