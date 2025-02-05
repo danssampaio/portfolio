a6f8d25a1eca84f4c29ad2ed189347581a258dfe
Dan Souza <darleyble3@gmail.com>
2025-02-04 23:43:19 -0300

    add highlighted projects in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { HeroSection } from "./components/pages/home/hero-section";
+import { HighlightedProjects } from "./components/pages/home/highlighted-projects";
 import { KnowTechs } from "./components/pages/home/know-techs";
 
 export default function Home() {
   return (
     <>
-    <div className="bg-hero-image bg-fixed">
-      <HeroSection />
-      <KnowTechs />
-    </div>
+      <div className="bg-hero-image bg-fixed">
+        <HeroSection />
+        <KnowTechs />
+        <HighlightedProjects />
+      </div>
     </>
   );
 }
