bc46726f69c497ec89481ca5c60884d9c1eb34cc
Dan Souza <darleyble3@gmail.com>
2025-02-04 23:45:26 -0300

    divider added in know techs

--- app/components/pages/home/know-techs/index.tsx
+++ app/components/pages/home/know-techs/index.tsx
@@ -1,11 +1,13 @@
 import { SectionTitle } from "@/app/components/section-title";
 import { KnowTech } from "./know-tech";
 import { technologies } from "@/app/components/pages/home/know-techs/technologies";
+import { HorizontalDivider } from "@/app/components/divider";
 
 export const KnowTechs = () => {
   return (
     <section className="container">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
+      <HorizontalDivider className="mb-16"/>
 
       <div className="grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-4">
         {technologies.map((tech, index) => (
