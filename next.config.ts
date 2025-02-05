1f4e5762507675cf0a338f47dc478bb46c94f581
Dan Souza <darleyble3@gmail.com>
2025-02-04 23:42:39 -0300

    new image domains added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com", 'media.graphassets.com'],
+  },
 };
 
 export default nextConfig;
