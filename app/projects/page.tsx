73a9e05f805b75667a6d76f64edd66ee51d764d3
Dan Souza <darleyble3@gmail.com>
2025-02-05 23:57:15 -0300

    project page

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import { PageIntroduction } from "../components/pages/projects/page-introduction";
+import { ProjectsList } from "../components/pages/projects/project-list";
+
+export default function Projects() {
+  return (
+    <>
+      <PageIntroduction />
+      <ProjectsList />
+    </>
+  );
+}
