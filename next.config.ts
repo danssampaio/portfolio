20d5eb62ebfc17118a7a700913b8be9f471ef3a4
Dan Souza <darleyble3@gmail.com>
2025-02-08 20:51:10 -0300

    hygraph domain added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["encrypted-tbn0.gstatic.com", 'media.graphassets.com', 'proex.uesc.br'],
+    domains: ["encrypted-tbn0.gstatic.com", 'media.graphassets.com', 'proex.uesc.br', 'us-west-2.graphassets.com'],
   },
 };
 
