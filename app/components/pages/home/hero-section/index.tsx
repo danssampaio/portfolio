6e9529c223154030f05e95ebf97046b1e11a5d78
Dan Souza <darleyble3@gmail.com>
2025-02-08 20:57:18 -0300

    adding hygraph hero section data

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,48 +1,18 @@
 "use client";
 
 import { Button } from "@/app/components/button";
+import { CMSIcon } from "@/app/components/cms-icon";
+import { RichText } from "@/app/components/rich-text";
 import { TechBadge } from "@/app/components/tech-bagde";
+import { HomePageInfo } from "@/app/types/pages-info";
 import Image from "next/image";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import { LuMail } from "react-icons/lu";
-import {
-  TbBrandGithub,
-  TbBrandInstagram,
-  TbBrandLinkedin,
-} from "react-icons/tb";
 
-const CONTACTS = [
-  {
-    url: "https://github.com/danssampaio",
-    icon: TbBrandGithub,
-  },
-  {
-    url: "https://www.instagram.com/dan.souza_s/",
-    icon: TbBrandInstagram,
-  },
-  {
-    url: "https://www.linkedin.com/in/dan-souza-sampaio-046170193/",
-    icon: TbBrandLinkedin,
-  },
-  {
-    url: "mailto:dan.souza.swork@gmail.com",
-    icon: LuMail,
-  },
-];
+type HomeSectionProps = {
+  homeInfo: HomePageInfo;
+};
 
-const TECHNOLOGIES = [
-  "HTML",
-  "CSS",
-  "Angular",
-  "Typescript",
-  "NodeJs",
-  "Java",
-  "Spring Boot",
-  "PostgreSQL",
-  "MySQL",
-];
-
-export const HeroSection = () => {
+export const HeroSection = ({ homeInfo }: HomeSectionProps) => {
   const HandleContact = () => {
     const contactSection = document.querySelector("#contact");
     if (contactSection) {
@@ -55,29 +25,25 @@ export const HeroSection = () => {
       <div className="container flex items-start justify-between flex-col-reverse md:flex-row">
         <div className="w-full lg:max-w-[530px]">
           <p className="font-mono text-[#1effff] sm:pt-5">
-            Olá pessoas, meu nome é{" "}
+            Olá pessoas, meu nome é
           </p>
-          <h2 className="text-4xl font-medium mt-2">Dan Souza</h2>
-          <p className="text-gray-400 text-justify my-6 text-sm sm:text-base">
-            Sou desenvolvedor web full stack, com foco no front-end, apaixonado
-            por criar interfaces intuitivas, funcionais e acessíveis. Formado em
-            Ciência da Computação, tenho experiência em desenvolvimento de
-            aplicações responsivas e interativas, sempre buscando a melhor
-            experiência para o usuário. Comunicativo e proativo, gosto de
-            transformar ideias em soluções eficientes e visualmente atraentes.
-          </p>
+          <h2 className="text-4xl font-medium mt-2">Darley(Dan) Souza</h2>
+          <div className="text-gray-400 text-justify my-6 text-sm sm:text-base">
+            <RichText content={homeInfo.introduction.raw} />
+          </div>
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[530px]">
-            {TECHNOLOGIES.map((tech, index) => (
-              <TechBadge key={index} name={tech} />
+            {homeInfo.technologies.map((tech, index) => (
+              <TechBadge key={index} name={tech.name} />
             ))}
           </div>
-          <div className="flex mt-4 lg:mt-10 sm:items-center sm:gap-5 flex-col sm:flex-row ">
+          <div className="flex gap-3 mt-4 lg:mt-10 lg:items-start sm:gap-5 flex-col sm:flex-row ">
             <Button onClick={HandleContact} className="w-max">
               Entre em contato
               <HiArrowNarrowRight size={18} />
             </Button>
-            <div className="flex items-center ms-2 h-20 gap-3 text-gray-600 justify-center">
-              {CONTACTS.map((contact, index) => (
+            <div className="flex w-fit items-center h-12 gap-3 text-gray-600 rounded-lg border border-[#1bc6c6] px-2">
+              <span className="text-gray-50">Siga-me nas redes sociais:</span>
+              {homeInfo.socials.map((contact, index) => (
                 <a
                   className="hover:text-gray-100 transition-colors"
                   href={contact.url}
@@ -85,7 +51,7 @@ export const HeroSection = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <contact.icon size={28} />
+                  <CMSIcon icon={contact.iconSvg} />
                 </a>
               ))}
             </div>
@@ -96,7 +62,7 @@ export const HeroSection = () => {
           width={320}
           height={304}
           priority
-          src="/images/profile-pic.jpg"
+          src={homeInfo.profilePicture.url}
           alt="Minha Foto"
         />
       </div>
