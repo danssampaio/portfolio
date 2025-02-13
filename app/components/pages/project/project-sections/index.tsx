969bb7874f3547f0001c7e7acdb6b808d747d4ad
Dan Souza <darleyble3@gmail.com>
2025-02-13 01:56:08 -0300

    integration project sections  added

--- app/components/pages/project/project-sections/index.tsx
+++ app/components/pages/project/project-sections/index.tsx
@@ -1,39 +1,36 @@
 "use client";
 
+import { ProjectSection } from "@/app/types/projects";
 import Image from "next/image";
 
-const sections = [
-  {
-    title: "Login",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqN3wWXAui-0ecpu9l47AgL6YCb53cYIqp_Q&s",
-  },
-  {
-    title: "Home",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqN3wWXAui-0ecpu9l47AgL6YCb53cYIqp_Q&s",
-  },
-];
+type ProjectSectionsProps = {
+  projectSections: ProjectSection[];
+};
 
-export const ProjectSections = () => {
+export const ProjectSections = ({ projectSections }: ProjectSectionsProps) => {
   return (
-    <section className="container grid grid-cols-1 my-12 md:my-32 gap-4 md:gap-16 lg:grid-cols-2">
-      {sections.map((section) => (
+    <section className="container my-12 md:my-32 flex flex-col gap-4 md:gap-32">
+      {projectSections.map((section) => (
         <div
           key={section.title}
-          className="flex flex-col items-center gap-6 md:gap-12"
+          className="flex flex-col items-center gap-3 md:gap-6"
         >
           <h2 className="text-2xl md:text-3xl font-medium text-neutral-300">
             {section.title}
           </h2>
-          <Image
-            src={section.image}
-            alt={`Imagem da sessão ${section.title}`}
-            width={1080}
-            height={672}
-            className="w-full aspect-auto rounded-lg object-cover"
-            unoptimized
-          />
+          <p className="text-neutral-400 text-center">
+            {section.description}
+          </p>
+          <div className="w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-auto rounded-lg">
+            <Image
+              src={section.image.url}
+              alt={`Imagem da sessão ${section.title}`}
+              width={1080}
+              height={672}
+              className="w-full h-auto object-cover"
+              unoptimized
+            />
+          </div>
         </div>
       ))}
     </section>
