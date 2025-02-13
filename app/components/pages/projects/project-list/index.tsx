97fbdc863b10fb81bd5417a5b98101494c03c4bc
Dan Souza <darleyble3@gmail.com>
2025-02-13 01:56:57 -0300

    integration project list added

--- app/components/pages/projects/project-list/index.tsx
+++ app/components/pages/projects/project-list/index.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-
 import { Link } from "@/app/components/link";
 import { ProjectCard } from "./project-card";
+import { Project } from "@/app/types/projects";
 
-export const ProjectsList = () => {
+type ProjectsProps = {
+  projects: Project[];
+};
+
+export const ProjectsList = ({ projects }: ProjectsProps) => {
   return (
     <section className="container py-16 grid grid-col-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
-      <Link href="/projects/PROEX">
-        <ProjectCard />
-      </Link>
+      {projects.map((project) => (
+        <Link key={project.title} href={`/projects/${project.slug}`}>
+          <ProjectCard project={project} />
+        </Link>
+      ))}
     </section>
   );
 };
