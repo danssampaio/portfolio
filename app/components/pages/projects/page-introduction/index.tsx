61b0995fdec14806d8a5551735b3b97c07e8944e
Dan Souza <darleyble3@gmail.com>
2025-05-08 20:36:59 -0300

    refactor:lazymotion animation added

--- app/components/pages/projects/page-introduction/index.tsx
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -3,31 +3,42 @@
 import { Link } from "@/app/components/link";
 import { SectionTitle } from "@/app/components/section-title";
 import { HiArrowNarrowLeft } from "react-icons/hi";
+import { LazyMotion, domAnimation } from "motion/react";
+import * as m from "motion/react-m";
 
 export const PageIntroduction = () => {
   return (
-    <section className="flex flex-col mt-16 items-center justify-center px-2 w-full h-[450px] lg:h-[630px]">
-      <SectionTitle
-        className="text-center items-center [&>h3]:text-4xl"
-        title="Meus Projetos"
-      />
-      <div className="flex flex-col items-center">
-        <p className="text-neutral-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
-          Aqui você encontrará alguns projetos em que atuei nos últimos anos.
-          Cada um deles é um marco da minha jornada profissional, desde o início
-          de minhas explorações com desenvolvimento front-end até a construção
-          de soluções completas em back-end.
-          <br />
-          Alguns projetos em que atuei não estarão aqui, por questão de
-          armazenamento em outras plataformas a quais não tenho acesso, ou já
-          não estão mais em uso, pois foram criados para se utilizar somente uma
-          ocasião específica.
-        </p>
-        <Link href="/">
-          <HiArrowNarrowLeft />
-          Voltar para Home
-        </Link>
-      </div>
-    </section>
+    <LazyMotion features={domAnimation}>
+      <m.section
+        className="flex flex-col mt-16 items-center justify-center px-2 w-full h-[450px] lg:h-[630px]"
+        initial={{ opacity: 0, y: 50 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0 }}
+        viewport={{ once: true }}
+      >
+        <SectionTitle
+          className="text-center items-center [&>h3]:text-4xl"
+          title="Meus Projetos"
+        />
+        <div className="flex flex-col items-center">
+          <p className="text-neutral-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
+            Aqui você encontrará alguns projetos em que atuei nos últimos anos.
+            Cada um deles é um marco da minha jornada profissional, desde o
+            início de minhas explorações com desenvolvimento front-end até a
+            construção de soluções completas em back-end.
+            <br />
+            Alguns projetos em que atuei não estarão aqui, por questão de
+            armazenamento em outras plataformas a quais não tenho acesso, ou já
+            não estão mais em uso, pois foram criados para se utilizar somente
+            uma ocasião específica.
+          </p>
+          <Link href="/">
+            <HiArrowNarrowLeft />
+            Voltar para Home
+          </Link>
+        </div>
+      </m.section>
+    </LazyMotion>
   );
 };
--- app/components/pages/projects/project-list/index.tsx
+++ app/components/pages/projects/project-list/index.tsx
@@ -3,6 +3,7 @@
 import { Link } from "@/app/components/link";
 import { ProjectCard } from "./project-card";
 import { Project } from "@/app/types/projects";
+import { domAnimation, LazyMotion, m } from "motion/react";
 
 type ProjectsProps = {
   projects: Project[];
@@ -10,12 +11,20 @@ type ProjectsProps = {
 
 export const ProjectsList = ({ projects }: ProjectsProps) => {
   return (
-    <section className="container py-16 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
-      {projects.map((project) => (
-        <Link key={project.title} href={`/projects/${project.slug}`}>
-          <ProjectCard project={project} />
-        </Link>
-      ))}
-    </section>
+    <LazyMotion features={domAnimation}>
+      <m.section
+        className="container py-16 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+      >
+        {projects.map((project) => (
+          <Link key={project.title} href={`/projects/${project.slug}`}>
+            <ProjectCard project={project} />
+          </Link>
+        ))}
+      </m.section>
+    </LazyMotion>
   );
 };
