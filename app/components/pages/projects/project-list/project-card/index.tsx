152ef629bc0b0d66ee378a982d41a1d0dbd96157
Dan Souza <darleyble3@gmail.com>
2025-05-28 18:02:40 -0300

    chore(style):update hover border color

--- app/components/pages/projects/project-list/project-card/index.tsx
+++ app/components/pages/projects/project-list/project-card/index.tsx
@@ -9,7 +9,7 @@ export const ProjectCard = ({ project }: ProjectProps) => {
   return (
     <div
       className="rounded-lg h-[360px] flex flex-col bg-neutral-800 overflow-hidden border-2
-     border-neutral-700 hover:border-cyan-600 opacity-80 hover:opacity-100 transition-all group"
+     border-neutral-700 hover:border-[#1bc6c6] opacity-80 hover:opacity-100 transition-all group"
     >
       <div className="w-full h-48 overflow-hidden">
         <Image
@@ -22,7 +22,7 @@ export const ProjectCard = ({ project }: ProjectProps) => {
         />
       </div>
       <div className="flex-1 flex flex-col p-4">
-        <strong className="font-semibold text-neutral-50/90 group-hover:text-cyan-600 transition-all">
+        <strong className="font-semibold text-neutral-50/90 group-hover:text-[#1bc6c6] transition-all">
           {project.title}
         </strong>
         <p className="mt-1 text-neutral-400 text-sm line-clamp-3">
